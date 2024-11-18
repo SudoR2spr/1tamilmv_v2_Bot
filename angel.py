64a804a2b7a8cdca71be4ac132a8473ceffc8d1d
WOODcraft <121882568+SudoR2spr@users.noreply.github.com>
2024-11-18 15:31:41 +0530

    Update Site Domain change ✅
    Site Domain change ✅

--- angel.py
+++ angel.py
@@ -77,7 +77,7 @@ def makeKeyboard(movie_list):
     return markup
 
 def tamilmv():
-    mainUrl = 'https://www.1tamilmv.wf/'
+    mainUrl = 'https://www.1tamilmv.at/'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
     }
